50849fed2dc3859c729f0935f97eda7a1172dd8a
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-09-09 21:59:05 +0530

    feat: add ApiError class for error handling and enhance registerUser validation

--- practise-6/src/controllers/user.controller.js
+++ practise-6/src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import {User} from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
@@ -12,6 +14,22 @@ const registerUser = asyncHandler(async (req, res) => {
     // return res
     const {fullName, email, password, username} = req.body
     console.log("email: ", email);
+
+    if(
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or : [{username}, {email}] 
+    })
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already existed")
+    }
 })
 
-export {registerUser};
+export {
+    registerUser,
+
+};
