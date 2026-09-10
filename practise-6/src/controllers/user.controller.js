d7472fea3ff68df9e854e51f943ddf084e15fd60
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-09-10 23:50:37 +0530

    feat: add image upload handling in registerUser and fix user existence check

--- practise-6/src/controllers/user.controller.js
+++ practise-6/src/controllers/user.controller.js
@@ -23,15 +23,15 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{username}, {email}] 
     })
     if(existedUser){
         throw new ApiError(409, "User with email or username already existed")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImagePath = req.files?.coverImage[0]?.path;
+const avatarLocalPath = req.files?.avatar?.[0]?.path;
+const coverImagePath = req.files?.coverImage?.[0]?.path;
 
     if (!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
