35e1bfeb615910b43e8733f13aaa90ac826326ef
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-09-14 02:20:28 +0530

    feat: implement JWT verification middleware and add logout functionality

--- practise-6/src/controllers/user.controller.js
+++ practise-6/src/controllers/user.controller.js
@@ -122,7 +122,32 @@ const loginUser = asyncHandler(async (req, res) => {
             "User logged In successfully"))
 })
 
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,{
+                $set: {
+                    refreshToken: undefined
+                }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out Succesfully"))
+})
+
 export {
     registerUser,
     loginUser,
+    logoutUser
 };
