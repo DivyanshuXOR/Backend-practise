fade3c651ba90af86e33433e9ea468c1158b83e3
DivyanshuXOR <sonidivyanshu200@gmail.com>
2026-09-15 23:05:27 +0530

    remove minor whitespaces

--- practise-6/src/controllers/user.controller.js
+++ practise-6/src/controllers/user.controller.js
@@ -97,7 +97,7 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 
     const user = await User.findOne({
-        $or: [{ username }, { email }]
+        $or: [{username}, {email}]
     })
 
     if (!user) {
